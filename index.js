1c32a35dc0933b9d9ada46a6fb95e08c4d5858cd
samhamm <samhamm@gmail.com>
2015-02-08 23:31:33 -0800

    Donegit status! This really was a fantastic tutorial.

--- index.js
+++ index.js
@@ -8,5 +8,6 @@
  handle['/'] = requestHandlers.start;
  handle['/start'] = requestHandlers.start;
  handle['/upload'] = requestHandlers.upload;
+ handle['/show'] = requestHandlers.show;
 
 server.start(router.route, handle);
